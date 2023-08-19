e3a297878a5541b86ba5047bdae5fbe5cf0bc33d
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-19 07:22:17 -0700

    mohamed 1st commit

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char *c);
+void _puts(char *str);
+int _prinf(int *format, ...);
+void _putnum(int num);
+
+#endif
