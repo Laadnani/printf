905f38691de2fe76b3bb90591104ce1753dd94d2
ogali26 <patienceogali@gmail.com>
2023-08-20 05:52:35 -0700

    patience ogali commit

--- putnum.c
+++ putnum.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
+/**
+ * printnum - functions that prints the numbers
+ * @num: numbers to be printed
+ * Return: number to print
+ */
+
 void printnum(int num)
 {
-	if(num < 0)
+	if (num < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
-	if(num / 10)
+	if (num / 10)
 	print_num(num / 10);
 	_putchar((num % 10) + '0');
 }
