21977fa2e1f5449961fde248f60f3f0e63230f6b
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-20 06:20:05 -0700

    mohamed

--- putnum.c
+++ putnum.c
@@ -8,12 +8,15 @@
 
 void printnum(int num)
 {
-	if (num < 0)
+	if (num)
 	{
-		_putchar('-');
-		num = -num;
+		if (num < 0)
+		{
+			_putchar('-');
+			num = -num;
+		}
+		if (num / 10)
+		printnum(num / 10);
+		_putchar((num % 10) + '0');
 	}
-	if (num / 10)
-	printnum(num / 10);
-	_putchar((num % 10) + '0');
 }
