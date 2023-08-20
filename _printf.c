a6ec09e252ff3c474f78076f75cc1120f6f2e637
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-20 05:36:43 -0700

    mohamed 3rd push

--- _printf.c
+++ _printf.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
+ * _printf - printf function mimic
+ * @format: format of the selector
  *
- *
+ * Return: returning the int i
  */
 int _printf(char *format, ...)
 {
 	char *str;
 	int i, j;
-	
+
 	va_list(args);
 	va_start(args, format);
-	for(i = 0; format[i]; i++ )
+	for (i = 0; format[i]; i++)
 	{
-		while(format[i] != '%')
+		while (format[i] != '%')
 		{
 			if (!format[i])
 			{
@@ -23,22 +25,31 @@ int _printf(char *format, ...)
 			i++;
 		}
 		i++;
-		switch(format[i])
+		switch (format[i])
 		{
-		case 'c': j = va_arg(args, int);
-				_putchar(j);
-				break;
-		case 's': str = va_arg(args, char*);
-				_puts(str);
-				break;
-		case 'i': j = va_arg(args, int);
-				printnum(format[i]);
-				break;
-		default : j = va_arg(args, int);
-			  _putchar(j);
-			  break;
+		case 'c':
+			j = va_arg(args, int);
+			_putchar(j);
+			break;
+#ifndef MAIN_H
+		case 's':
+			str = va_arg(args, char*);
+			_puts(str);
+			break;
+		case 'i':
+			j = va_arg(args, int);
+			printnum(format[i]);
+			break;
+		case '%':
+			_putchar(37);
+			break;
+		default:
+			j = va_arg(args, int);
+			_putchar(j);
+			break;
 		}
 	}
 	va_end(args);
 	return (i);
+#endif
 }
--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * _putchar - returning the ASCII value
+ * @c: character value
+ * Return: int value
+ */
+
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
