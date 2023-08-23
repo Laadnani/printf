7f7bceb53c92d02740b26cbbc7991dce495659cd
ogali26 <patienceogali@gmail.com>
2023-08-23 11:41:08 -0700

    Pats commit 6

--- width.c
+++ width.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string where the argument will be printed
- * @i: arguments to be printed.
- * @args: arguments.
- *
- * Return: width.
- */
+* get_width - Calculates the width for printing
+* @format: Formatted string where the argument will be printed
+* @i: arguments to be printed.
+* @args: arguments.
+*
+* Return: width.
+*/
 int get_width(const char *format, int *i, va_list args)
 {
-    int next_index;
-    int width = 0;
+int next_index;
+int width = 0;
 
-    for (next_index = *i + 1; format[next_index] != '\0'; next_index++)
-    {
-        if (is_digit(format[next_index]))
-        {
-            width *= 10;
-            width += format[next_index] - '0';
-        }
-        else if (format[next_index] == '*')
-        {
-            next_index++;
-            width = va_arg(args, int);
-            break;
-        }
-        else
-            break;
-    }
+for (next_index = *i + 1; format[next_index] != '\0'; next_index++)
+{
+if (is_digit(format[next_index]))
+{
+width *= 10;
+width += format[next_index] - '0';
+}
+else if (format[next_index] == '*')
+{
+next_index++;
+width = va_arg(args, int);
+break;
+}
+else
+break;
+}
 
-    *i = next_index - 1;
+*i = next_index - 1;
 
-    return (width);
+return (width);
 }
