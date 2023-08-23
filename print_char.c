85b2650f6b472c1a6eaee1bf4c75197603f02780
ogali26 <patienceogali@gmail.com>
2023-08-23 10:11:18 -0700

    pats 15th commit

--- print_char.c
+++ print_char.c
@@ -3,18 +3,18 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * p_char - Prints a char
- * @types: args a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  finds if a flag specifier is active
- * @width: Width
- * @precision: Precision 
- * @size: Size 
- * Return: Number of chars printed
- */
+* p_char - Prints a char
+* @types: args a of arguments
+* @buffer: Buffer array to handle print
+* @flags:  finds if a flag specifier is active
+* @width: Width
+* @precision: Precision 
+* @size: Size 
+* Return: Number of chars printed
+*/
 int p_char(va_list types, char buffer[],int flags, int width, int precision, int size)
 {
-    char c = va_arg(types, int);
+char c = va_arg(types, int);
 
-    return (handle_write_char(c, buffer, flags, width, precision, size));
+return (handle_write_char(c, buffer, flags, width, precision, size));
 }
