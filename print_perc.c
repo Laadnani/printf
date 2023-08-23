9f3abdbf9c7b9eaf58eb566f7d22124e85539dff
ogali26 <patienceogali@gmail.com>
2023-08-23 10:42:09 -0700

    pats 18th commit

--- print_perc.c
+++ print_perc.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * p_percent - Prints a percent sign
- * @types: list of arguments
- * @buffer: Buffer array to handle print
- * @flags:  finds if a flag specifier is active
- * @width:  width.
- * @precision: Precision
- * @size: Size
- * Return: Number of chars printed
- */
+* p_percent - Prints a percent sign
+* @types: list of arguments
+* @buffer: Buffer array to handle print
+* @flags:  finds if a flag specifier is active
+* @width:  width.
+* @precision: Precision
+* @size: Size
+* Return: Number of chars printed
+*/
 int p_percent(va_list types, char buffer[],
-                  int flags, int width, int precision, int size)
+int flags, int width, int precision, int size)
 {
-    UNUSED(types);
-    UNUSED(buffer);
-    UNUSED(flags);
-    UNUSED(width);
-    UNUSED(precision);
-    UNUSED(size);
-    return (write(1, "%", 1));
+UNUSED(types);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+return (write(1, "%", 1));
 }
