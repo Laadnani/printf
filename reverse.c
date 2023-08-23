a9c82889ed40e99cb8d4dfa2009e20f75a24b0fc
ogali26 <patienceogali@gmail.com>
2023-08-23 11:23:05 -0700

    pats commit 2

--- reverse.c
+++ reverse.c
@@ -1,48 +1,48 @@
 #include "main.h"
 /**
- * p_rev - Prints reverse string.
- * @types: agrguments
- * @buffer: Buffer array to handle print
- * @flags:  finds if a flag specifier is active
- * @width:  width
- * @precision: Precision
- * @size: Size
- * Return: Numbers of chars printed
- */
+* p_rev - Prints reverse string.
+* @types: agrguments
+* @buffer: Buffer array to handle print
+* @flags:  finds if a flag specifier is active
+* @width:  width
+* @precision: Precision
+* @size: Size
+* Return: Numbers of chars printed
+*/
 
 int p_rev(va_list types, char buffer[], int flags, int width, int precision, int size)
 {
-    char *str;
-    int i, count = 0;
+char *str;
+int i, count = 0;
 
-    UNUSED(buffer);
-    UNUSED(flags);
-    UNUSED(width);
-    UNUSED(size);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(size);
 
-    str = va_arg(types, char *);
+str = va_arg(types, char *);
 
-    if (!str)
-    {
-        UNUSED(precision);
+if (!str)
+{
+UNUSED(precision);
 
-        str = "(Null)";
-    }
-    for (i = 0; str[i]; i++)
+str = "(Null)";
+}
+for (i = 0; str[i]; i++)
 	{
-        	if (str[1] == '\0')
-        	{
-            		count += write(1, "%r", 2);
-            		return(count);
-        	}
+	if (str[1] == '\0')
+	{
+		count += write(1, "%r", 2);
+		return (count);
+	}
 	}
 
-    for (i = i - 1; i >= 0; i--)
-    {
-        char z = str[i];
+for (i = i - 1; i >= 0; i--)
+{
+char z = str[i];
 
-        write(1, &z, 1);
-        count++;
-    }
-    return (count);
+write(1, &z, 1);
+count++;
+}
+return (count);
 }
